c234718a15ba566ea7a0f674ab3e43a516c8ffcf
xudafeng <xudafeng@126.com>
2014-12-18 11:55:45 +0800

    Add icon

--- iscanner_ios/AboutController.m
+++ iscanner_ios/AboutController.m
@@ -21,7 +21,12 @@
 }
 
 - (void)setAbout {
-  UITextView *text1 = [[UITextView alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, self.view.frame.size.height - 100)];
+  UIImageView *imageView1 = [[UIImageView alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 220) / 2, 100, 220, 220)];
+  imageView1.backgroundColor = [UIColor colorWithPatternImage: [UIImage imageNamed:@"icon.png"]];
+  imageView1.alpha = 0.1;
+  imageView1.layer.masksToBounds = YES;
+  [self.view addSubview:imageView1];
+  UITextView *text1 = [[UITextView alloc] initWithFrame:CGRectMake(0, 400, self.view.frame.size.width, 100)];
   text1.text = @"iScanner v1.0\nby xdf";
   text1.font = [UIFont systemFontOfSize: 20.0];
   text1.textAlignment = NSTextAlignmentCenter;
